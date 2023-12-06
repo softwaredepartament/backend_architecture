54e7b294fef52b42fe80f502dafcfde29df2a7b8
ShodiyevF <fayzulloh6757@gmail.com>
2023-12-06 10:44:31 +0500

    add multiple validation to fileupload

--- src/middleware/fileupload.middleware.js
+++ src/middleware/fileupload.middleware.js
@@ -1,26 +1,53 @@
 const { internalErrorCatcher } = require('../shared/logger/logger.internal');
-const path = require('path');
-const fs = require('fs');
 const { returnResponse } = require('../lib/express.function');
 const { errors } = require('../lib/httpException');
+const path = require('path');
+const fs = require('fs');
 
-function fileuploadMiddleware(reqFileKeyName, allowedFileTypes, allowedFileSize) {
+function fileuploadMiddleware(reqFileKeyName, allowedFileTypes, allowedFileSize, isRequired = true) {
     try {
         return (req, res, next) => {
-            const checkFileExists = req.files[reqFileKeyName];
-            if (!checkFileExists) {
-                return returnResponse(res, 403, `Expected name ${reqFileKeyName}`, errors.UPLOAD_ERROR);
-            }
 
-            const checkFileType = allowedFileTypes.find(type => type === checkFileExists.mimetype);
-            if (!checkFileType) {
-                return returnResponse(res, 403, `Expected type ${allowedFileTypes.join(' — ')}`, errors.UPLOAD_ERROR);
-            }
+            console.log(typeof reqFileKeyName);
+            if (typeof reqFileKeyName == 'object') {
+                for (const requiredFile of reqFileKeyName) {
+                    const findFile = req.files ? Object.keys(req.files).find(file => file === requiredFile) : false
 
-            const checkFileSize = allowedFileSize * 1024 * 1024 > checkFileExists.size;
-            console.log(checkFileSize);
-            if (!checkFileSize) {
-                return returnResponse(res, 403, `Max size ${allowedFileSize} MB`, errors.UPLOAD_ERROR);
+                    if (!isRequired ? findFile : false || isRequired) {
+                        if (!findFile) {
+                            return returnResponse(res, 403, `Expected name ${requiredFile}`, errors.UPLOAD_ERROR);
+                        }
+
+                        const checkFileType = allowedFileTypes.find(type => type === req.files[findFile].mimetype);
+                        if (!checkFileType) {
+                            return returnResponse(res, 403, `Expected type ${allowedFileTypes.join(' — ')}`, errors.UPLOAD_ERROR);
+                        }
+    
+                        const checkFileSize = allowedFileSize * 1024 * 1024 > req.files[findFile].size;
+                        if (!checkFileSize) {
+                            return returnResponse(res, 403, `Max size ${allowedFileSize} MB`, errors.UPLOAD_ERROR);
+                        }
+                    }
+                }
+            } else {
+                console.log('adsasd');
+                if (!isRequired ? (req.files ? req.files[reqFileKeyName] : false) : false || isRequired) {
+                    console.log('kirdi');
+                    const checkFileExists = req.files ? req.files[reqFileKeyName] : false;
+                    if (!checkFileExists) {
+                        return returnResponse(res, 403, `Expected name ${reqFileKeyName}`, errors.UPLOAD_ERROR);
+                    }
+
+                    const checkFileType = allowedFileTypes.find(type => type === checkFileExists.mimetype);
+                    if (!checkFileType) {
+                        return returnResponse(res, 403, `Expected type ${allowedFileTypes.join(' — ')}`, errors.UPLOAD_ERROR);
+                    }
+
+                    const checkFileSize = allowedFileSize * 1024 * 1024 > checkFileExists.size;
+                    if (!checkFileSize) {
+                        return returnResponse(res, 403, `Max size ${allowedFileSize} MB`, errors.UPLOAD_ERROR);
+                    }
+                }
             }
 
             next();
--- src/module/users/users.index.js
+++ src/module/users/users.index.js
@@ -1,14 +1,13 @@
+const { USER_PROFILE_IMG_NAME, USER_PROFILE_IMG_TYPES, USER_PROFILE_IMG_SIZE } = require('./users.config');
+const { fileuploadMiddleware } = require('../../middleware/fileupload.middleware');
 const validationMiddleware = require('../../middleware/validation.middleware');
 const { getUsersDto } = require('../../dto/users.dto');
 const { usersCtrl } = require('./users.ctrl');
-const { fileuploadMiddleware } = require('../../middleware/fileupload.middleware');
-const { USER_PROFILE_IMG_NAME, USER_PROFILE_IMG_TYPES, USER_PROFILE_IMG_SIZE } = require('./users.config');
 
 const express = require('express').Router();
 
 express.post(
     '/api/users/:id',
-    fileuploadMiddleware(USER_PROFILE_IMG_NAME, USER_PROFILE_IMG_TYPES, USER_PROFILE_IMG_SIZE),
     fileuploadMiddleware(USER_PROFILE_IMG_NAME, USER_PROFILE_IMG_TYPES, USER_PROFILE_IMG_SIZE),
     validationMiddleware(getUsersDto, 'params'),
     (req, res) => usersCtrl(req, res),
